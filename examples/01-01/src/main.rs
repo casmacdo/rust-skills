09b5882c5cff89c8313a9f53869f69ac51debc94
casmacdo <casmac@tuta.io>
2022-11-16 18:58:31 -0500

    fixed an example

--- examples/01-01/src/main.rs
+++ examples/01-01/src/main.rs
@@ -1,4 +1,3 @@
 fn main() { // declare function, name it, and set arguments
     println!("Cassidy has giant quads"); // use println! macro and give string argument
-    let quads = String::new()
 } // close expression
